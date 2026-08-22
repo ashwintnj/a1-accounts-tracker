b0ad4eef1572616a014fb23e5050c3d178dabc74
Ashwin <ashwin.ab@cognizant.com>
2026-08-22 10:44:55 +0530

    minor_changes

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,8 +1,8 @@
 export const DEFAULT_BANKS = [
-    { id: crypto.randomUUID(), name: 'Indian Bank Current Account', opening: 0, closing: 0 },
-    { id: crypto.randomUUID(), name: 'Indian Bank Savings Account', opening: 0, closing: 0 },
-    { id: crypto.randomUUID(), name: 'Jio Payments Bank', opening: 0, closing: 0 },
-    { id: crypto.randomUUID(), name: 'Bank of Baroda', opening: 0, closing: 0 }
+    { id: crypto.randomUUID(), name: 'Indian Bank Current Account (xx2496)', opening: 0, closing: 0 },
+    { id: crypto.randomUUID(), name: 'Indian Bank Savings Account (xx6794)', opening: 0, closing: 0 },
+    { id: crypto.randomUUID(), name: 'Jio Payments Bank (xx0693)', opening: 0, closing: 0 },
+    { id: crypto.randomUUID(), name: 'Bank of Baroda (xx2860)', opening: 0, closing: 0 }
 ];
 
 export const DEFAULT_RECEIVED_LABEL = 'GPAY';
